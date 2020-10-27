58ed4f6f4d49f991f5ea66161366c22ab317ad30
Harun <67702854+hrndgc@users.noreply.github.com>
2020-10-27 18:28:57 +0300

    Details page completed

--- src/pages/RestaurantDetail.js
+++ src/pages/RestaurantDetail.js
@@ -1,16 +1,46 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet, Dimensions, Image} from 'react-native';
 
 const RestaurantDetail = (props) => {
     const {selectedRestaurant} = props.route.params;
     
     return(
-        <SafeAreaView>
-            <View>
-                <Text>Restaurant Detail</Text>
+        <SafeAreaView style={{flex: 1}}>
+            <View style={StyleSheet.container}>
+                <Text style={styles.name}>Restaurant Detail</Text>
+
+                <Image
+                    style={styles.image}
+                    source={{uri: selectedRestaurant.image_url}}
+                />
+
+                <View> style={styles.infoContainer}
+                     <Text style={styles.infoText}>{selectedRestaurant.address}</Text>
+                </View>
+
+                <View> style={styles.infoContainer}
+                     <Text style={styles.infoText}>{selectedRestaurant.area}</Text>
+                </View>
+
+                <View> style={styles.infoContainer}
+                     <Text style={styles.infoText}>{selectedRestaurant.postal_code}</Text>
+                </View>
             </View>
         </SafeAreaView>
     );
 }
 
 export {RestaurantDetail}
+
+const styles =StyleSheet.create({
+    container: {flex: 1, padding: 10},
+    name: {fontWeight: '300', fontSize: 30},
+    image: {height: Dimensions.get('window').height / 3},
+    infoContainer: {
+        backgroundColor: '#29b6f6',
+        padding: 10,
+        margin: 5,
+        borderRadius: 5
+    },
+    infoText: {color: 'white', fontWeight: 'bold'}
+})
